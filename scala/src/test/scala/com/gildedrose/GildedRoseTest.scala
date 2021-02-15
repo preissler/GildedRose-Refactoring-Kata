36837868cf095d73e241c7bcb508b12cd6a6bff7
Luciano Preissler <luciano.preissler@gmail.com>
2021-02-15 18:15:06 -0300

    adding tests for Quality and SellIn

--- scala/src/test/scala/com/gildedrose/GildedRoseTest.scala
+++ scala/src/test/scala/com/gildedrose/GildedRoseTest.scala
@@ -1,13 +1,39 @@
 package com.gildedrose
 
-import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
 
-class GildedRoseTest  extends AnyFlatSpec with Matchers {
-      it should "foo" in {
-        val items = Array[Item](new Item("foo", 0, 0))
-        val app = new GildedRose(items)
-        app.updateQuality()
-        app.items(0).name should equal ("fixme")
+class GildedRoseTest  extends AnyWordSpec with Matchers {
+      "Quality" should {
+        "decrease 1 " in {
+          val items = Array[Item](new Item("foo", 0, 1))
+          val app = new GildedRose(items)
+          app.updateQuality()
+          app.items(0).quality shouldBe 0
+        }
+
+        "increase 1" in {
+          val items = Array[Item](new Item("Aged Brie", 7, 1))
+          val app = new GildedRose(items)
+          app.updateQuality()
+          app.items(0).quality shouldBe 2
+        }
+        "increase 2" in {
+          val items = Array[Item](new Item("Aged Brie", 0, 1))
+          val app = new GildedRose(items)
+          app.updateQuality()
+          app.items(0).quality shouldBe 3
+        }
+
+
+
+      }
+      "Sell In" should {
+        "decrease 1" in {
+          val items = Array[Item](new Item("foo", 1, 1))
+          val app = new GildedRose(items)
+          app.updateQuality()
+          app.items(0).sellIn shouldBe 0
+        }
       }
 }
